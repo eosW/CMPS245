173cb1211a788f638e8e692fce5621c4f9a6e1d4
endofsamsara <end.wrm@gmail.com>
2017-01-25 16:40:07 -0800

    pre processing

--- hw1/preproc.py
+++ hw1/preproc.py
@@ -11,7 +11,7 @@ def pre_processing(readfilename,writefileneme):
         tweets = (row[-1] for row in reader)
         no_tag = (re.sub('<[^>]+>','',tweet) for tweet in tweets)
         no_link = (re.sub('(?:http|ftp|www)\S+','',tweet) for tweet in no_tag)
-        no_at = (re.sub('@','',tweet) for tweet in no_link)
+        no_at = (re.sub('@','',tweet).strip() for tweet in no_link)
         no_hash = (re.sub('(?: #\S+)+$','',tweet) for tweet in no_at)
         tokens = (tknzr.tokenize(tweet.lower()) for tweet in no_hash)
         writer.writerows(tokens)
